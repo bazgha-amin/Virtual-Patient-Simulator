37a6859d9a83b4048b0d6c5a9aa606321e3f1e90
Bazgha Amin <bazgha_amin@yahoo.com>
2020-07-04 00:28:25 +0500

    Initial Commit

---
+++ Assets/Scripts/OpenInvestigationPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenInvestigationPanel : MonoBehaviour
+{
+   public GameObject Panel;
+
+   public void InvestigationPanel()
+   {
+       if (Panel != null)
+       {
+           bool isActive = Panel.activeSelf;
+           Panel.SetActive (!isActive);
+       }
+   }
+}
